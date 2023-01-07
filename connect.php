f15956e44cb7b5777dea8b5f868ae6edc8388dbc
Kanokporn1 <thanason1003@gmail.com>
2023-01-07 13:00:40 +0700

    edit connect

--- connect.php
+++ connect.php
@@ -7,8 +7,7 @@
     /*$username = "root";
     $password = "12345678";
     $dbname = "wed2111";*/
-
-    // Create connection
+    
     $conn = new mysqli($servername, $username, $password, $dbname);
     mysqli_set_charset($conn,"utf8");
     if ($conn->connect_error) {
