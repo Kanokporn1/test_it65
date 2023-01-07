86bebd811ce1fdc7a2893e8703f53a698516f27e
Kanokporn1 <thanason1003@gmail.com>
2023-01-07 09:54:52 +0700

    new many file

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+    $servername = "localhost";
+    /*$username = "it65309010018";
+    $password = "it12345";
+    $dbname = "it65309010018";*/
+
+    $username = "root";
+    $password = "12345678";
+    $dbname = "wed2111";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    mysqli_set_charset($conn,"utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.bundle.min.js"></script>
+
+  
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
+    
+    <div class="p-5 bg-primary text-white text-center">
+    <h1>ข้อมูลนักเรียน</h1>
+    <p>วิทยาลัยเทคนิคชลบุรี</p> 
+    </div>
